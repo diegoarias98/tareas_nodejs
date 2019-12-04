f9b06a709a49aebed86b6d0444045899456c2e8c
diegoarias98 <dariass1@est.ups.edu.ec>
2019-12-04 09:21:25 -0500

    Commit Inicial

---
+++ config/yargs.js
@@ -0,0 +1,32 @@
+const descripcion = {
+    demand: true,
+    alias: 'd',
+    desc: "Descripción de la tarea por hacer"
+};
+
+const completado = {
+    default: true,
+    alias: 'c',
+    desc: "Marca como completada o pendiente la tarea"
+};
+
+const argv = require('yargs')
+    .command('crear', 'Crear una tarea', {
+        descripcion
+    })
+    .command('actualizar', 'Actualiza una tarea', {
+        descripcion,
+        completado
+    })
+    .command('borrar', 'Elimina una tarea', {
+        descripcion
+    })
+    .command('listar', 'Lista las tareas',{
+        completado
+    })
+    .help()
+    .argv;
+
+module.exports = {
+    argv
+}
